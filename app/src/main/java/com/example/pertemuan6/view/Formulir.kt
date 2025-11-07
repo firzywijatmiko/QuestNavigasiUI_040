0270d411977830893701e68f29f5f504ce872ef2
firzywijatmiko <emailkamu@example.com>
2025-11-07 15:07:53 +0700

    mebahakan fum formulir

---
+++ app/src/main/java/com/example/pertemuan6/view/Formulir.kt
@@ -0,0 +1,48 @@
+package com.example.pertemuan6.view
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+
+@OptIn(markerClass = ExperimentalMaterial3Api::class)
+@Composable
+fun Formulir(
+    jenis: List<String> = listOf("Laki-laki", "Perempuan"),
+    onSubmitButtonClick: () -> Unit
+){
+    Scaffold(modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.home), color = color.white) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.teal_700))
+            )}
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .width(250.dp),
+                label = { Text(text = "Nama Lengkap") },
+                onValueChange = { }
+            )
+        }
+    }
+}
