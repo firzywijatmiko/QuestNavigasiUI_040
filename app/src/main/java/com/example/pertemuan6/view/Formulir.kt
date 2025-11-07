6b4661f4aac71a49ea45c9fe9252996dff0646cb
firzywijatmiko <emailkamu@example.com>
2025-11-07 15:40:55 +0700

    menambahkan outlinet file

--- app/src/main/java/com/example/pertemuan6/view/Formulir.kt
+++ app/src/main/java/com/example/pertemuan6/view/Formulir.kt
@@ -3,11 +3,17 @@ package com.example.pertemuan6.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
 import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Label
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
@@ -72,7 +78,23 @@ fun Formulir(
                 .padding(all = 20.dp)
                 .width(width = 250.dp),
                 thickness = 1.dp,
-                color = Color.Red)
+                color = Color.Red
+            )
+            OutlinedTextField(
+                value = "",
+                singleLine = true,
+                modifier = Modifier
+                    .width(width = 250.dp),
+                Label =  {Text(text = "Alamat") },
+                onValueChang = {},
+            )
+            Spacer(modifier = Modifier.height(height = 30.dp))
+            Button(
+                modifier = Modifier.fillMaxWidth(fraction = 1f)
+                    .padding(all = 25.dp),
+                onClick = onSubmitButtonClick
+            ){
+
         }
     }
 }
