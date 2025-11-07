7b33f091c2da1e2d052bda5a56f43b50474cfbf0
firzywijatmiko <emailkamu@example.com>
2025-11-07 15:27:43 +0700

    mebahakan horizontal fileid

--- app/src/main/java/com/example/pertemuan6/view/Formulir.kt
+++ app/src/main/java/com/example/pertemuan6/view/Formulir.kt
@@ -4,7 +4,12 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.DividerDefaults.Thickness
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -12,8 +17,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalOf
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.pertemuan6.R
+
 
 @OptIn(markerClass = ExperimentalMaterial3Api::class)
 @Composable
@@ -24,11 +34,16 @@ fun Formulir(
     Scaffold(modifier = Modifier,
         topBar = {
             TopAppBar(
-                title = { Text(text = stringResource(id = R.string.home), color = color.white) },
+                title = { Text(text = stringResource(id = R.string.home), color = Color.White) },
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     containerColor = colorResource(id = R.color.teal_700))
             )}
-
+    ) { isiRuang ->
+        Column(
+            modifier = Modifier.padding(paddingValues = isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
             OutlinedTextField(
                 value = "",
                 singleLine = true,
@@ -38,6 +53,26 @@ fun Formulir(
                 label = { Text(text = "Nama Lengkap") },
                 onValueChange = { }
             )
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp), thickness = Thickness,color = Color.Red)
+            Row {
+                jenis.forEach {
+                    item->
+                    Row(verticalAlignment = Alignment.CenterHorizontally){
+                        RadioButton(
+                            selected = false,
+                            onClick = {item}
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
+            HorizontalDivider(modifier = Modifier
+                .padding(all = 20.dp)
+                .width(width = 250.dp),
+                thickness = 1.dp,
+                color = Color.Red)
         }
     }
 }
